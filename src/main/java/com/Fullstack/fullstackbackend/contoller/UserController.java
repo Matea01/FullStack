6431ff0e005cbc483c75c1e8ea4cc09248c61aa1
MAtea01 <mateamilutin01@gmail.com>
2023-06-01 22:18:50 +0200

    Connecting to database

--- src/main/java/com/Fullstack/fullstackbackend/contoller/UserController.java
+++ src/main/java/com/Fullstack/fullstackbackend/contoller/UserController.java
@@ -4,13 +4,27 @@ import com.Fullstack.fullstackbackend.model.User;
 import com.Fullstack.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller // This means that this class is a Controller
-@RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
+import java.util.List;
+
+@RestController  // This means that this class is a Controller
+//@RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
+@CrossOrigin("http://localhost:3000")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+    return userRepo.save(newUser);
+    }
+
+    @GetMapping("users")
+    List<User> gelAllUsers(){
+        return userRepo.findAll();
+    }
+
 
 }
