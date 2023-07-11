d1cef3de73cb76bc7fca578cca5ffca09876ac0b
MAtea01 <mateamilutin01@gmail.com>
2023-07-11 21:20:53 +0200

    test

--- src/main/java/com/Fullstack/fullstackbackend/FullstackBackendApplication.java
+++ src/main/java/com/Fullstack/fullstackbackend/FullstackBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class FullstackBackendApplication {
-
+//new comment
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackBackendApplication.class, args);
 	}
